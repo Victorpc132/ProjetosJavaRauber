4cc87f148fcf487b5c3db03645ff90963a6b7aeb
Victorpc132 <vmano9251@gmail.com>
2025-10-06 11:53:41 -0300

    ExPara Prova

---
+++ Projetos Java Victor/ExMatriz07/src/main/java/com/mycompany/exmatriz07/ExMatriz07.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exmatriz07;
+
+/**
+ *
+ * @author alunos
+ */
+public class ExMatriz07 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
